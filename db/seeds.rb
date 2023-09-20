53d0e3b5f582e96a3d4bc18a526f67bbbcd24eb7
Maxime Oudin <oudinmaxime@gmail.com>
2023-09-20 13:48:41 +0200

    seed done with 4 travels

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Travel.destroy_all
+
+puts 'destroy all done'
+
+User.create!(email: "toto@mail.com", password: "azerty")
+Travel.create!(
+  title: "Venise",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores a animi nulla veritatis numquam ea assumenda facere cumque hic itaque reprehenderit, magnam sint mollitia perferendis. Non, totam iure. Nemo.",
+  image_url: "https://media.routard.com/image/81/3/venise-grand-canal.1479813.142.jpg",
+  rating: 2
+)
+
+Travel.create!(
+  title: "Berlin",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores a animi nulla veritatis numquam ea assumenda facere cumque hic itaque reprehenderit, magnam sint mollitia perferendis. Non, totam iure. Nemo.",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Berlin_Brandenburger_Tor_Nacht.jpg/800px-Berlin_Brandenburger_Tor_Nacht.jpg",
+  rating: 4
+)
+
+Travel.create!(
+  title: "Venise bis",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores a animi nulla veritatis numquam ea assumenda facere cumque hic itaque reprehenderit, magnam sint mollitia perferendis. Non, totam iure. Nemo.",
+  image_url: "https://media.routard.com/image/81/3/venise-grand-canal.1479813.142.jpg",
+  rating: 1
+)
+
+Travel.create!(
+  title: "Berlin bis",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae asperiores a animi nulla veritatis numquam ea assumenda facere cumque hic itaque reprehenderit, magnam sint mollitia perferendis. Non, totam iure. Nemo.",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Berlin_Brandenburger_Tor_Nacht.jpg/800px-Berlin_Brandenburger_Tor_Nacht.jpg",
+  rating: 5
+)
+
+puts '4 travels created'
